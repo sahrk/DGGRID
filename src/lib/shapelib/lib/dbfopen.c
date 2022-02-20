dcae6c5892508791cf25fd788274ca5ab331618e
Richard Barnes <rbarnes@umn.edu>
2022-02-19 20:24:51 -0800

    Remove extraneous output in shapelib and fix a truncated string

--- src/lib/shapelib/lib/dbfopen.c
+++ src/lib/shapelib/lib/dbfopen.c
@@ -1403,7 +1403,6 @@ static int DBFWriteAttribute(DBFHandle psDBF, int hEntity, int iField,
 {
     int	       	i, j, nRetResult = TRUE;
     unsigned char	*pabyRec;
-    char	szSField[XBASE_FLD_MAX_WIDTH+1], szFormat[20];
 
 /* -------------------------------------------------------------------- */
 /*	Is this a valid record?						*/
@@ -1465,18 +1464,21 @@ static int DBFWriteAttribute(DBFHandle psDBF, int hEntity, int iField,
       {
         int		nWidth = psDBF->panFieldSize[iField];
 
+        char szSField[XBASE_FLD_MAX_WIDTH+1];
         if( (int) sizeof(szSField)-2 < nWidth )
             nWidth = sizeof(szSField)-2;
 
+        char szFormat[20];
         snprintf( szFormat, sizeof(szFormat), "%%%d.%df",
                     nWidth, psDBF->panFieldDecimals[iField] );
         CPLsnprintf(szSField, sizeof(szSField), szFormat, *((double *) pValue) );
+        szSField[sizeof(szSField)-1] = '\0';
         if( (int) strlen(szSField) > psDBF->panFieldSize[iField] )
         {
             szSField[psDBF->panFieldSize[iField]] = '\0';
             nRetResult = FALSE;
         }
-        strncpy((char *) (pabyRec+psDBF->panFieldOffset[iField]),
+        memcpy((char *) (pabyRec+psDBF->panFieldOffset[iField]),
             szSField, strlen(szSField) );
         break;
       }
@@ -2075,7 +2077,7 @@ DBFReorderFields( DBFHandle psDBF, int* panMap )
     panFieldSizeNew = (int *) calloc(sizeof(int),  psDBF->nFields);
     panFieldDecimalsNew = (int *) calloc(sizeof(int), psDBF->nFields);
     pachFieldTypeNew = (char *) calloc(sizeof(char), psDBF->nFields);
-    pszHeaderNew = (char*) malloc(sizeof(char) * XBASE_FLDHDR_SZ * 
+    pszHeaderNew = (char*) malloc(sizeof(char) * XBASE_FLDHDR_SZ *
                                   psDBF->nFields);
 
     /* shuffle fields definitions */
