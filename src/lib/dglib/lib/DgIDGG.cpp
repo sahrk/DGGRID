fbad33ba42333e93ccfe327e2fd031b4dd091117
Kevin Sahr <sahrkm@gmail.com>
2022-03-03 21:33:40 -0800

    memfix

--- src/lib/dglib/lib/DgIDGG.cpp
+++ src/lib/dglib/lib/DgIDGG.cpp
@@ -35,6 +35,7 @@
 //#include <dglib/DgSeriesConverter.h>
 #include <dglib/DgRadixString.h>
 #include <dglib/DgBoundedIDGG.h>
+#include <dglib/DgIDGGSBase.h>
 //#include <dglib/DgProjISEA.h>
 //#include <dglib/DgProjFuller.h>
 
@@ -42,17 +43,17 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-DgIDGG::DgIDGG (const DgIDGGSBase* dggs, const DgGeoSphRF& geoRF, const DgGeoCoord& vert0,
-       long double azDegs, unsigned int aperture, int res, const string& name,
-       DgGridTopology gridTopo, DgGridMetric gridMetric, const string& projType,
+DgIDGG::DgIDGG (const DgIDGGSBase* dggs, unsigned int aperture, int res, const string& name,
+       DgGridTopology gridTopoIn, DgGridMetric gridMetric, unsigned int precision,
        bool isMixed43, int numAp4, bool isSuperfund, int sfRes, bool isApSeq,
-       const DgApSeq& apSeq, unsigned int precision)
-   : DgIDGGBase (dggs, geoRF, aperture, res, name, gridTopo, gridMetric, precision),
-     geoRF_(geoRF), vert0_(vert0), azDegs_(azDegs),
-     projType_(projType), isApSeq_ (isApSeq), apSeq_ (apSeq), isMixed43_(isMixed43),
+       const DgApSeq& apSeq)
+   : DgIDGGBase (dggs, dggs->geoRF(), aperture, res, name, gridTopoIn, gridMetric, precision),
+     isApSeq_ (isApSeq), apSeq_ (apSeq), isMixed43_(isMixed43),
      numAp4_(numAp4), isSuperfund_(isSuperfund), sfRes_ (sfRes)
 {
-   initialize();
+   // hex grid init takes place in DgHexIDGG constructor
+   if (gridTopoIn != Hexagon)
+      initialize();
 
 } // DgIDGG::DgIDGG
 
@@ -60,14 +61,13 @@ DgIDGG::DgIDGG (const DgIDGGSBase* dggs, const DgGeoSphRF& geoRF, const DgGeoCoo
 DgIDGG::DgIDGG (const DgIDGG& rfIn)
    : DgIDGGBase (NULL, rfIn.geoRF(), rfIn.aperture(), rfIn.res(), rfIn.name(),
                  rfIn.gridTopo(), rfIn.gridMetric(), rfIn.precision()),
-        geoRF_(rfIn.geoRF()), vert0_(rfIn.vert0()),
-        azDegs_(rfIn.azDegs()),
-        projType_ (rfIn.projType()), isApSeq_ (rfIn.isApSeq()),
         apSeq_ (rfIn.apSeq()),
         isMixed43_(rfIn.isMixed43()), numAp4_(rfIn.numAp4()),
         isSuperfund_(rfIn.isSuperfund()), sfRes_(rfIn.sfRes())
 {
-   initialize();
+   // hex grid init takes place in DgHexIDGG constructor
+   if (gridTopo() != Hexagon)
+      initialize();
 
 } // DgIDGG::DgIDGG
 
