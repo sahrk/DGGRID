c221efdefa609bce1280c53f8a7188b07719f872
sahrkm <sahrkm@gmail.com>
2024-04-26 16:40:15 -0700

    Z7 source files

---
+++ src/lib/dglib/lib/DgZ7RF.cpp
@@ -0,0 +1,244 @@
+/*******************************************************************************
+    Copyright (C) 2023 Kevin Sahr
+
+    This file is part of DGGRID.
+
+    DGGRID is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DGGRID is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*******************************************************************************/
+////////////////////////////////////////////////////////////////////////////////
+//
+// DgZ7RF.cpp: DgZ7RF class implementation
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <inttypes.h>
+#include <string.h>
+
+#include <dglib/DgZ7RF.h>
+#include <dglib/DgZ7StringRF.h>
+#include <dglib/DgIDGGBase.h>
+#include <dglib/DgRadixString.h>
+
+/** max ZORDER resolution */
+#define MAX_ZORDER_RES 30
+
+/** The number of bits in a Z7 index. */
+#define ZORDER_NUM_BITS 64
+
+/** The bit offset of the max resolution digit in a Z7 index. */
+#define ZORDER_MAX_OFFSET 63
+
+/** The bit offset of the quad number in a Z7 index. */
+#define ZORDER_QUAD_OFFSET 60
+
+/** 1's in the 4 quad number bits, 0's everywhere else. */
+#define ZORDER_QUAD_MASK ((uint64_t)(15) << ZORDER_QUAD_OFFSET)
+
+/** 0's in the 4 mode bits, 1's everywhere else. */
+#define ZORDER_QUAD_MASK_NEGATIVE (~ZORDER_QUAD_MASK)
+
+/** The number of bits in a single ZORDER resolution digit. */
+#define ZORDER_PER_DIGIT_OFFSET 2
+
+/** 1's in the 2 bits of highest res digit bits, 0's everywhere else. */
+#define ZORDER_DIGIT_MASK ((uint64_t)(3))
+
+/** Gets the integer quad number of a Z7 index. */
+#define ZORDER_GET_QUADNUM(z) ((int)((((z)&ZORDER_QUAD_MASK) >> ZORDER_QUAD_OFFSET)))
+
+/** Sets the integer mode of z to v. */
+#define ZORDER_SET_QUADNUM(z, v) \
+    (z) = (((z)&ZORDER_QUAD_MASK_NEGATIVE) | (((uint64_t)(v)) << ZORDER_QUAD_OFFSET))
+
+/** Gets the resolution res integer digit of z. */
+#define ZORDER_GET_INDEX_DIGIT(z, res)                                        \
+    ((int)((((z) >> ((MAX_ZORDER_RES - (res)) * ZORDER_PER_DIGIT_OFFSET)) & \
+                  ZORDER_DIGIT_MASK)))
+
+/** Sets the resolution res digit of z to the integer digit */
+#define ZORDER_SET_INDEX_DIGIT(z, res, digit)                                  \
+    (z) = (((z) & ~((ZORDER_DIGIT_MASK                                        \
+                       << ((MAX_ZORDER_RES - (res)) * ZORDER_PER_DIGIT_OFFSET)))) | \
+            (((uint64_t)(digit))                                            \
+             << ((MAX_ZORDER_RES - (res)) * ZORDER_PER_DIGIT_OFFSET)))
+
+////////////////////////////////////////////////////////////////////////////////
+const DgZ7Coord DgZ7Coord::undefDgZ7Coord(0xffffffffffffffff);
+
+////////////////////////////////////////////////////////////////////////////////
+const char*
+DgZ7RF::str2add (DgZ7Coord* add, const char* str,
+                         char delimiter) const
+{
+   char delimStr[2];
+   delimStr[0] = delimiter;
+   delimStr[1] = '\0';
+
+   char* tmpStr = new char[strlen(str) + 1];
+   strcpy(tmpStr, str);
+   char* tok = strtok(tmpStr, delimStr);
+
+   // convert to a unit64_t
+   uint64_t val = 0;
+   if (!sscanf(tok, "%" PRIx64, &val))
+      report("DgZ7RF::str2add(): invalid ZORDER index", DgBase::Fatal);
+
+   if (!add) add = new DgZ7Coord();
+   add->setValue(val);
+
+   unsigned long offset = strlen(tok) + 1;
+   delete[] tmpStr;
+   if (offset >= strlen(str)) return 0;
+   else return &str[offset];
+
+} // const char* DgZ7RF::str2add
+
+////////////////////////////////////////////////////////////////////////////////
+string
+DgZ7Coord::valString (void) const
+{
+   const int maxStrSize = 17; // max 16 digits plus 1 for the null terminator
+   char str[maxStrSize];
+   snprintf(str, maxStrSize, "%016" PRIx64, value_);
+
+   return string(str);
+
+} // const string& DgZ7RF::valString
+
+////////////////////////////////////////////////////////////////////////////////
+DgZ7StringtoZ7Converter::DgZ7StringtoZ7Converter
+                (const DgRF<DgZ7StringCoord, long long int>& from,
+                 const DgRF<DgZ7Coord, long long int>& to)
+   : DgConverter<DgZ7StringCoord, long long int, DgZ7Coord, long long int> (from, to)
+{
+   // validate the to/from RF's
+   const DgZ7StringRF* zsRF = dynamic_cast<const DgZ7StringRF*>(&fromFrame());
+   if (!zsRF) {
+      report("DgZ7StringtoZ7Converter::DgZ7StringtoZ7Converter(): "
+         " fromFrame not of type DgZ7StringRF", DgBase::Fatal);
+   }
+
+   const DgZ7RF* zRF = dynamic_cast<const DgZ7RF*>(&toFrame());
+   if (!zRF) {
+      report("DgZ7StringtoZ7Converter::DgZ7StringtoZ7Converter(): "
+         " toFrame not of type DgZ7RF", DgBase::Fatal);
+   }
+
+   if (zsRF->aperture() != zRF->aperture() || zsRF->res() != zRF->res()) {
+      report("DgZ7StringtoZ7Converter::DgZ7StringtoZ7Converter(): "
+         " fromFrame and toFrame apertures or resolutions do not match", DgBase::Fatal);
+   }
+
+   // store the res
+   res_ = zRF->res();
+
+} // DgZ7StringtoZ7Converter::DgZ7StringtoZ7Converter
+
+////////////////////////////////////////////////////////////////////////////////
+DgZ7Coord
+DgZ7StringtoZ7Converter::convertTypedAddress (const DgZ7StringCoord& addIn) const
+{
+   string addstr = addIn.valString();
+   uint64_t z = 0;
+
+   // first get the quad number and add to the val
+   string qstr = addstr.substr(0, 2);
+   if (qstr[0] == '0') // leading 0
+      qstr = qstr.substr(1, 1);
+   int quadNum = std::stoi(qstr);
+   ZORDER_SET_QUADNUM(z, quadNum);
+
+   int index = 2; // skip the two quad digits
+
+   // the rest is the radix string
+   string radStr = addstr.substr(index);
+
+   // now get the digits
+   int r = 1;
+   for (const char& digit: radStr) {
+      if (r > res_)
+         report("DgZ7StringtoZ7Converter::convertTypedAddress(): "
+         " incoming index exceeds converter resolution", DgBase::Fatal);
+
+      int d = digit - '0'; // convert to int
+      ZORDER_SET_INDEX_DIGIT(z, r, d);
+      r++;
+   }
+
+   DgZ7Coord coord;
+   coord.setValue(z);
+
+   return coord;
+
+} // DgZ7Coord DgZ7StringtoZ7Converter::convertTypedAddress
+
+////////////////////////////////////////////////////////////////////////////////
+DgZ7ToZ7StringConverter::DgZ7ToZ7StringConverter
+                (const DgRF<DgZ7Coord, long long int>& from,
+                 const DgRF<DgZ7StringCoord, long long int>& to)
+        : DgConverter<DgZ7Coord, long long int, DgZ7StringCoord, long long int> (from, to)
+{
+   // validate the to/from RF's
+   const DgZ7RF* zRF = dynamic_cast<const DgZ7RF*>(&fromFrame());
+   if (!zRF) {
+      report("DgZ7ToZ7StringConverter::DgZ7ToZ7StringConverter(): "
+         " fromFrame not of type DgZ7RF", DgBase::Fatal);
+   }
+
+   const DgZ7StringRF* zsRF = dynamic_cast<const DgZ7StringRF*>(&toFrame());
+   if (!zsRF) {
+      report("DgZ7ToZ7StringConverter::DgZ7ToZ7StringConverter(): "
+         " toFrame not of type DgZ7StringRF", DgBase::Fatal);
+   }
+
+   if (zsRF->aperture() != zRF->aperture() || zsRF->res() != zRF->res()) {
+      report("DgZ7ToZ7StringConverter::DgZ7ToZ7StringConverter(): "
+         " fromFrame and toFrame apertures or resolutions do not match", DgBase::Fatal);
+   }
+
+   // store the res
+   res_ = zRF->res();
+
+} // DgZ7StringtoZ7Converter::DgZ7StringtoZ7Converter
+
+////////////////////////////////////////////////////////////////////////////////
+DgZ7StringCoord
+DgZ7ToZ7StringConverter::convertTypedAddress (const DgZ7Coord& addIn) const
+{
+   uint64_t z = addIn.value();
+
+   int quadNum = ZORDER_GET_QUADNUM(z);
+   string s = dgg::util::to_string(quadNum, 2);
+
+   for (int r = 1; r <= res_; r++) {
+      // get the integer digit
+      char d = ZORDER_GET_INDEX_DIGIT(z, r);
+      // convert to char
+      d += '0';
+      // append to index string
+      s += d;
+   }
+
+   DgZ7StringCoord zStr;
+   zStr.setValString(s);
+
+   return zStr;
+
+} // DgZ7StringCoord DgZ7ToZ7StringConverter::convertTypedAddress
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
---
+++ src/lib/dglib/lib/DgZ7StringRF.cpp
@@ -0,0 +1,275 @@
+/*******************************************************************************
+    Copyright (C) 2023 Kevin Sahr
+
+    This file is part of DGGRID.
+
+    DGGRID is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DGGRID is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*******************************************************************************/
+////////////////////////////////////////////////////////////////////////////////
+//
+// DgZ7StringRF.cpp: DgZ7StringRF class implementation
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <string.h>
+
+#include <dglib/DgZ7StringRF.h>
+#include <dglib/DgIDGGBase.h>
+#include <dglib/DgIDGGSBase.h>
+#include <dglib/DgRadixString.h>
+
+////////////////////////////////////////////////////////////////////////////////
+const DgZ7StringCoord DgZ7StringCoord::undefDgZ7StringCoord("99");
+
+////////////////////////////////////////////////////////////////////////////////
+const char*
+DgZ7StringRF::str2add (DgZ7StringCoord* add, const char* str,
+                         char delimiter) const
+{
+   if (!add) add = new DgZ7StringCoord();
+
+   char delimStr[2];
+   delimStr[0] = delimiter;
+   delimStr[1] = '\0';
+
+   char* tmpStr = new char[strlen(str) + 1];
+   strcpy(tmpStr, str);
+   char* tok = strtok(tmpStr, delimStr);
+
+   add->setValString(tok);
+
+   unsigned long offset = strlen(tok) + 1;
+   delete[] tmpStr;
+
+   if (offset >= strlen(str)) return 0;
+   else return &str[offset];
+
+} // const char* DgZ7StringRF::str2add
+
+////////////////////////////////////////////////////////////////////////////////
+DgQ2DItoZ7StringConverter::DgQ2DItoZ7StringConverter
+                (const DgRF<DgQ2DICoord, long long int>& from,
+                 const DgRF<DgZ7StringCoord, long long int>& to)
+        : DgConverter<DgQ2DICoord, long long int, DgZ7StringCoord, long long int> (from, to),
+          pIDGG_ (NULL), effRes_ (0), effRadix_ (0)
+{
+   pIDGG_ = dynamic_cast<const DgIDGGBase*>(&fromFrame());
+   if (!pIDGG_) {
+      report("DgQ2DItoZ7StringConverter::DgQ2DItoZ7StringConverter(): "
+         " fromFrame not of type DgIDGGBase", DgBase::Fatal);
+   }
+
+   const DgZ7StringRF* zRF = dynamic_cast<const DgZ7StringRF*>(&toFrame());
+   if (!zRF) {
+      report("DgQ2DItoZ7StringConverter::DgQ2DItoZ7StringConverter(): "
+         " toFrame not of type DgZ7StringRF", DgBase::Fatal);
+   }
+
+   if (IDGG().dggs()->aperture() != zRF->aperture() || IDGG().res() != zRF->res()) {
+      report("DgQ2DItoZ7StringConverter::DgQ2DItoZ7StringConverter(): "
+         "fromFrame and toFrame apertures or resolutions do not match", DgBase::Fatal);
+   }
+
+   if (IDGG().gridTopo() != Hexagon || IDGG().dggs()->aperture() != 3) {
+      report("DgQ2DItoZ7StringConverter::DgQ2DItoZ7StringConverter(): "
+         "only implemented for aperture 3 hexagon grids", DgBase::Fatal);
+   }
+
+   effRadix_ = 3;
+
+   // effRes_ is the number of Class I resolutions
+   effRes_ = (IDGG().res() + 1) / 2;
+
+} // DgQ2DItoZ7StringConverter::DgQ2DItoZ7StringConverter
+
+////////////////////////////////////////////////////////////////////////////////
+DgZ7StringCoord
+DgQ2DItoZ7StringConverter::convertTypedAddress (const DgQ2DICoord& addIn) const
+{
+   string qstr = dgg::util::to_string(addIn.quadNum(), 2);
+   string addstr = qstr;
+
+/*
+   if (IDGG().dggs()->aperture() == 3) {
+      dgcout << "Class " << ((IDGG().isClassI()) ? "I" : "II") << endl;
+   }
+*/
+   if (effRes_ > 0) {
+//dgcout << "** addIn " << addIn << endl;
+      DgRadixString rs1(effRadix_, (int) addIn.coord().i(), effRes_);
+      DgRadixString rs2(effRadix_, (int) addIn.coord().j(), effRes_);
+//dgcout << "rs1 " << rs1 << endl;
+//dgcout << "rs2 " << rs2 << endl;
+
+      string digits1 = rs1.digits();
+      string digits2 = rs2.digits();
+
+      int n1 = (int) digits1.length();
+      int n2 = (int) digits2.length();
+
+      // pad shorter one with leading 0's
+      if (n2 < n1) {
+         for (int i = 0; i < (n1 - n2); i++) digits2 = "0" + digits2;
+      } else {
+         for (int i = 0; i < (n2 - n1); i++) digits1 = "0" + digits1;
+      }
+
+      // convert each coordinate pair into Z7 digits
+
+      // table of Z7 digits corresponding to the
+      // class I (i,j) coordinates
+      static const string z3digits[3][3] = {
+       // j = 0     1     2
+           {"00", "22", "21"}, // i == 0
+           {"01", "02", "20"}, // i == 1
+           {"12", "10", "11"}  // i == 2
+      };
+
+      for (unsigned int i = 0; i < digits1.length(); i++) {
+         addstr = addstr +
+              z3digits[digits1[i] - '0'][digits2[i] - '0'];
+      }
+
+      // trim last digit if Class II
+      if (!IDGG().isClassI() && addstr.length())
+         addstr.pop_back();
+   }
+
+   DgZ7StringCoord zorder;
+   zorder.setValString(addstr);
+//dgcout << "zorder " << zorder << endl;
+
+   return zorder;
+
+} // DgZ7StringCoord DgQ2DItoZ7StringConverter::convertTypedAddress
+
+////////////////////////////////////////////////////////////////////////////////
+DgZ7StringToQ2DIConverter::DgZ7StringToQ2DIConverter
+                (const DgRF<DgZ7StringCoord, long long int>& from,
+                 const DgRF<DgQ2DICoord, long long int>& to)
+        : DgConverter<DgZ7StringCoord, long long int, DgQ2DICoord, long long int> (from, to),
+          pIDGG_ (NULL), effRes_ (0), effRadix_ (0)
+{
+   pIDGG_ = dynamic_cast<const DgIDGGBase*>(&toFrame());
+   if (!pIDGG_) {
+      report("DgZ7StringToQ2DIConverter::DgZ7StringToQ2DIConverter(): "
+         " toFrame not of type DgIDGGBase", DgBase::Fatal);
+   }
+
+   const DgZ7StringRF* zRF = dynamic_cast<const DgZ7StringRF*>(&fromFrame());
+   if (!zRF) {
+      report("DgQ2DItoZ7StringConverter::DgQ2DItoZ7StringConverter(): "
+         " fromFrame not of type DgZ7StringRF", DgBase::Fatal);
+   }
+
+   if (IDGG().dggs()->aperture() != zRF->aperture() || IDGG().res() != zRF->res()) {
+      report("DgQ2DItoZ7StringConverter::DgQ2DItoZ7StringConverter(): "
+         " fromFrame and toFrame apertures or resolutions do not match", DgBase::Fatal);
+   }
+
+   if (IDGG().gridTopo() != Hexagon || IDGG().dggs()->aperture() != 3) {
+      report("DgZ7StringToQ2DIConverter::DgZ7StringToQ2DIConverter(): "
+         "only implemented for aperture 3 hexagon grids", DgBase::Fatal);
+   }
+
+   effRes_ = IDGG().res();       // effective resolution
+   effRadix_ = 3;
+   // effRes_ is the number of Class I resolutions
+   effRes_ = (IDGG().res() + 1) / 2;
+
+} // DgQ2DItoZ7StringConverter::DgQ2DItoZ7StringConverter
+
+////////////////////////////////////////////////////////////////////////////////
+DgQ2DICoord
+DgZ7StringToQ2DIConverter::convertTypedAddress (const DgZ7StringCoord& addIn) const
+{
+   string addstr = addIn.valString();
+
+   // first get the quad number
+   string qstr = addstr.substr(0, 2);
+   if (qstr[0] == '0') // leading 0
+      qstr = qstr.substr(1, 1);
+   int quadNum = std::stoi(qstr);
+
+   // res 0 is just the quad number
+   if (effRes_ == 0)
+      return DgQ2DICoord(quadNum, DgIVec2D(0, 0));
+
+   int index = 2; // skip the two quad digits
+
+   // the rest is the Z7 digit string
+   string z3str = addstr.substr(index);
+
+//dgcout << "z3str in: " << z3str;
+
+   // adjust if Class II (odd res)
+   if (z3str.length() % 2)
+      z3str += "0";
+//dgcout << " adjusted: " << z3str << endl;
+
+   // build the digit string for i and j from the two-digit
+   // z3 codes
+   string radStr1 = "";
+   string radStr2 = "";
+   for (int i = 0; i < z3str.length(); i += 2) {
+      string z3code = z3str.substr(i, 2);
+      if (z3code == "00") {
+         radStr1 += "0";
+         radStr2 += "0";
+      } else if (z3code == "22") {
+         radStr1 += "0";
+         radStr2 += "1";
+      } else if (z3code == "21") {
+         radStr1 += "0";
+         radStr2 += "2";
+      } else if (z3code == "01") {
+         radStr1 += "1";
+         radStr2 += "0";
+      } else if (z3code == "02") {
+         radStr1 += "1";
+         radStr2 += "1";
+      } else if (z3code == "20") {
+         radStr1 += "1";
+         radStr2 += "2";
+      } else if (z3code == "12") {
+         radStr1 += "2";
+         radStr2 += "0";
+      } else if (z3code == "10") {
+         radStr1 += "2";
+         radStr2 += "1";
+      } else if (z3code == "11") {
+         radStr1 += "2";
+         radStr2 += "2";
+      }
+//      dgcout << "z3code: " << z3code << " radStr1: " << radStr1
+//             << " radStr2: " << radStr2 << endl;
+   }
+
+   DgRadixString rad1(effRadix_, radStr1);
+   DgRadixString rad2(effRadix_, radStr2);
+
+//   dgcout << "qstr: " << qstr << " rad1: " << rad1 << " rad2: " << rad2 << endl;
+
+   DgQ2DICoord q2di(quadNum, DgIVec2D(rad1.value(), rad2.value()));
+   //dgcout << "q2di: " << q2di << endl;
+
+   return q2di;
+
+} // DgQ2DICoord DgZ7StringToQ2DIConverter::convertTypedAddress
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
