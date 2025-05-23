810ca990daab7f057404a21e630d02d0915d3e7c
sahrkm <sahrkm@gmail.com>
2025-05-23 11:35:26 -0700

    added apexfunc dir

---
+++ src/apps/working/apexfunc/include/apexFuncs.h
@@ -0,0 +1,33 @@
+/*******************************************************************************
+    Copyright (C) 2025 Kevin Sahr
+
+    This file is part of DGGRID.
+
+    DGGRID is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DGGRID is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*******************************************************************************/
+////////////////////////////////////////////////////////////////////////////////
+//
+// appexFuncs.h: function prototypes for appex functions
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef APPEXFUNCS_H
+#define APPEXFUNCS_H
+
+////////////////////////////////////////////////////////////////////////////////
+// function prototypes
+int doSomething (void);
+
+////////////////////////////////////////////////////////////////////////////////
+#endif
---
+++ src/apps/working/apexfunc/lib/apexFuncs.cpp
@@ -0,0 +1,99 @@
+/*******************************************************************************
+    Copyright (C) 2025 Kevin Sahr
+
+    This file is part of DGGRID.
+
+    DGGRID is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DGGRID is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*******************************************************************************/
+////////////////////////////////////////////////////////////////////////////////
+//
+// apexFuncs.cpp: apex function implementations
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <dglib/DgIDGGS4H.h>
+#include "apexFuncs.h"
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////
+int doSomething (void) {
+
+   ///// create the DGG /////
+
+   // create the reference frame (RF) conversion network
+   DgRFNetwork net0;
+
+   // create the geodetic reference frame
+   // reference frames must be created dynamically using makeRF
+   // they will be deleted by the Network
+   const DgGeoSphRF& geoRF = *(DgGeoSphRF::makeRF(net0, "GS0"));
+
+   // create the ISEA4H grid system with resolutions 0-9; requires a
+   // fixed icosahedron vertex and edge azimuth
+   DgGeoCoord vert0(11.25L, 58.28252559L, false); // args: lon, lat, isRadians
+   long double azimuth = 0.0L;
+
+   // all DGGS's must be created using a factory makeRF method
+   // the DGGS is memory managed by the DgRFNetwork
+   const DgIDGGS4H* idggsPtr = DgIDGGS4H::makeRF(net0, geoRF, vert0, azimuth, 10);
+   const DgIDGGS4H& idggs = *idggsPtr;
+
+   // get the resolution 7 dgg from the dggs
+   const DgIDGG& dgg = idggs.idgg(7);
+   cout << dgg.gridStats() << endl;
+
+   //////// now use the DGG /////////
+
+   ///// given a point in lon/lat, get the cell it lies within /////
+
+   // first create a DgLocation in geoRF coordinates
+   DgGeoCoord geoAddress(-122.7083, 42.1947, false);
+   DgLocation* thePt = geoRF.makeLocation(geoAddress);
+   cout << "the point " << *thePt << endl;
+
+   // converting the point location to the dgg RF determines which cell it's in
+   dgg.convert(thePt);
+   cout << "* lies in cell " << *thePt << endl;
+
+   // we can get the cell's vertices, which are defined in geoRF
+   DgPolygon verts;
+   int ptsPerEdgeDensify = 3;
+   dgg.setVertices(*thePt, verts, ptsPerEdgeDensify);
+   cout << "* with densified cell boundary:\n" << verts << endl;
+
+   // we can get the cell's center point by converting the cell back to geoRF
+   geoRF.convert(thePt);
+   cout << "* and cell center point:" << *thePt << endl;
+
+   // we can extract the coordinates into primitive data types
+   const DgGeoCoord& centCoord = *geoRF.getAddress(*thePt);
+   double latRads = centCoord.lat();
+   double lonRads = centCoord.lon();
+   cout << "* center point lon,lat in radians: "
+        << lonRads << ", " << latRads << endl;
+
+   const DgGeoCoord& firstVert = *geoRF.getAddress(verts[0]);
+   double latDegs = firstVert.latDegs();
+   double lonDegs = firstVert.lonDegs();
+   cout << "* first boundary vertex lon,lat in degrees: "
+        << lonDegs << ", " << latDegs << endl;
+
+   delete thePt;
+
+   return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
